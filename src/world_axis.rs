19ed12dd73af32b7a4e7f7124b98a8bf16f4ae80
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-07-02 17:30:54 -0400

    Renamed to world_axis

--- src/world_axis.rs
+++ src/world_axis.rs
@@ -1,10 +1,12 @@
-pub enum Axis {
+use glam::DVec3;
+
+pub enum WorldAxis {
     X(glam::DVec3),
     Y(glam::DVec3),
     Z(glam::DVec3)
 }
 
-impl Axis {
+impl WorldAxis {
     fn rotate_dvec3(point_to_rotate: &mut glam::DVec3, rotation_axis: &glam::DVec3, angle: &f64) -> () {
         if rotation_axis.length() != 0f64 {
             let scaled_axis: glam::DVec3 = rotation_axis.normalize();
@@ -14,29 +16,34 @@ impl Axis {
     }
 
     pub fn rotate(self: &mut Self, rotation_axis: &glam::DVec3, angle: &f64) {
-        let (Axis::X(orientation) | Axis::Y(orientation) | Axis::Z(orientation)) = self;
-        Axis::rotate_dvec3(orientation, rotation_axis, angle);
+        let (WorldAxis::X(orientation) | WorldAxis::Y(orientation) | WorldAxis::Z(orientation)) = self;
+        WorldAxis::rotate_dvec3(orientation, rotation_axis, angle);
+    }
+
+    pub fn orientation(self: &Self) -> &DVec3 {
+        let (WorldAxis::X(orientation) | WorldAxis::Y(orientation) | WorldAxis::Z(orientation)) = self;
+        return orientation;
     }
 
     fn color(self: &Self) -> sdl2::pixels::Color {
         return match self {
-            Axis::X(_) => sdl2::pixels::Color::RED,
-            Axis::Z(_) => sdl2::pixels::Color::GREEN,
-            Axis::Y(_) => sdl2::pixels::Color::BLUE
+            WorldAxis::X(_) => sdl2::pixels::Color::RED,
+            WorldAxis::Z(_) => sdl2::pixels::Color::GREEN,
+            WorldAxis::Y(_) => sdl2::pixels::Color::BLUE
         };
     }
 
-    pub fn default_world_axes() -> [Axis; 3] {
+    pub fn default_world_axes() -> [WorldAxis; 3] {
         return [
-            Axis::X(glam::DVec3::new(1.0, 0.0, 0.0)),
-            Axis::Y(glam::DVec3::new(0.0, 1.0, 0.0)),
-            Axis::Z(glam::DVec3::new(0.0, 0.0, 1.0))
+            WorldAxis::X(glam::DVec3::new(1.0, 0.0, 0.0)),
+            WorldAxis::Y(glam::DVec3::new(0.0, 1.0, 0.0)),
+            WorldAxis::Z(glam::DVec3::new(0.0, 0.0, 1.0))
         ];
     }
 
     fn to_sdl_point(self: &Self, window_width: &f64, window_height: &f64) -> sdl2::rect::Point {
         // TODO: add perspective projection
-        let (Axis::X(orientation) | Axis::Y(orientation) | Axis::Z(orientation)) = self;
+        let (WorldAxis::X(orientation) | WorldAxis::Y(orientation) | WorldAxis::Z(orientation)) = self;
         return sdl2::rect::Point::new(
             ((orientation.x * 400f64) + window_width) as i32,
             ((orientation.y * 400f64) + window_height) as i32
